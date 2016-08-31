d2db4ce85b49d0ded014c75287404d3a767edf21
Dan Burton <danburton.email@gmail.com>
2016-08-31 14:29:47 -0700

    Add mutate fn and state atom

--- src/example/client.cljs
+++ src/example/client.cljs
@@ -6,20 +6,28 @@
 (def foo "foo")
 
 (def names-list
-  ["Rebecca" "Jesse" "Molly"])
+  '("Rebecca" "Jesse" "Molly"))
+
+(defn parser-mutate [env k params]
+  (when (= k 'prepend-name)
+    (let [state (:state env)]
+      {:action #(swap! state update :names conj (:name params))})))
 
 (defn parser-read [env k params]
   (when (= k :names)
-    (let [limit (:limit params)]
-      {:value (take limit names-list)})))
+    (let [limit (:limit params)
+          state (:state env)]
+      {:value (take limit (:names @state))})))
 
 (def parser
   (om/parser
-    {:read parser-read}))
+    {:read parser-read
+     :mutate parser-mutate}))
 
 (def reconciler
   (om/reconciler
-    {:parser parser}))
+    {:state {:names names-list}
+     :parser parser}))
 
 ;; https://github.com/omcljs/om/wiki/Quick-Start-(om.next)
 (om/defui HelloWorld
