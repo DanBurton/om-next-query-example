b60c7613f30a4e2911b2caa2cd610c5f85859114
Dan Burton <danburton.email@gmail.com>
2016-08-30 22:09:41 -0700

    minimal project.clj

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject om-next-example-project "0.1.0-SNAPSHOT"
+  :description "An example om.next app"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.225"]])
