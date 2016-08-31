39a7677338d17e4b47ca35a24e8339da3bab49a8
Dan Burton <danburton.email@gmail.com>
2016-08-31 15:00:21 -0700

    Add remote and fake send

--- src/example/client.cljs
+++ src/example/client.cljs
@@ -3,6 +3,22 @@
             [om.next :as om]
             [sablono.core :as sablono]))
 
+
+(defn fake-remote-parser-read [env k _params]
+  (case k
+    :server-time
+    {:value (str (js/Date))}
+
+    nil))
+
+(def fake-remote-parser
+  (om/parser {:read fake-remote-parser-read}))
+
+(defn send [{query :remote} cb]
+  (cb (fake-remote-parser nil query)))
+
+
+
 (def foo "foo")
 
 (def names-list
@@ -14,10 +30,18 @@
       {:action #(swap! state update :names conj (:name params))})))
 
 (defn parser-read [env k params]
-  (when (= k :names)
+  (case k
+    :names
     (let [limit (:limit params)
           state (:state env)]
-      {:value (take limit (:names @state))})))
+      {:value (take limit (:names @state))})
+
+    :server-time
+    (let [{:keys [state ast]} env]
+      {:value (get @state k)
+       :remote ast})
+
+    nil))
 
 (def parser
   (om/parser
@@ -27,7 +51,8 @@
 (def reconciler
   (om/reconciler
     {:state {:names names-list}
-     :parser parser}))
+     :parser parser
+     :send send}))
 
 ;; https://github.com/omcljs/om/wiki/Quick-Start-(om.next)
 (om/defui HelloWorld
@@ -37,16 +62,19 @@
 
   static om/IQuery
   (query [_]
-    '[(:names {:limit ?limit})])
+    '[(:names {:limit ?limit})
+      :server-time])
 
   Object
   (render [this]
-    (let [names (:names (om/props this))]
-    (sablono/html
-      [:div (str "Hello, "
-                (first names)
-                (when (second names)
-                  (str " and " (second names))))]))))
+    (let [{:keys [names server-time]} (om/props this)]
+      (sablono/html
+        [:div
+         [:div (str "Hello, "
+                    (first names)
+                    (when (second names)
+                      (str " and " (second names))))]
+         [:div (str "Server time: " server-time)]]))))
 
 (def hello (om/factory HelloWorld))
 
