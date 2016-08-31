b02cafcbd8bb65324b0d900d3d2ea87f1990f5e2
Dan Burton <danburton.email@gmail.com>
2016-08-31 00:18:36 -0700

    Quick Start with Sablono: render

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject example "0.1.0-SNAPSHOT"
   :description "An example om.next app"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.225"]]
+                 [org.clojure/clojurescript "1.9.225"]
+                 [org.omcljs/om "1.0.0-alpha41"]
+                 [sablono "0.7.4"]]
   :profiles {:dev {:source-paths ["dev" "src"]
                    :dependencies [[com.cemerick/piggieback "0.2.1"]
                                   [figwheel-sidecar "0.5.4-7"]]
--- src/example/client.cljs
+++ src/example/client.cljs
@@ -1,6 +1,18 @@
-(ns example.client)
+(ns example.client
+  (:require [goog.dom :as gdom]
+            [om.next :as om]
+            [sablono.core :as sablono]))
 
 (def foo "foo")
 
+;; https://github.com/omcljs/om/wiki/Quick-Start-(om.next)
+(om/defui HelloWorld
+  Object
+  (render [this]
+    (sablono/html
+      [:div "Hello, defui"])))
+
+(def hello (om/factory HelloWorld))
+
 (defn init []
-  (js/document.write "Hello, Figwheel!"))
+  (js/ReactDOM.render (hello) (gdom/getElement "app")))
