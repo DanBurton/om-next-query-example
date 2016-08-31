a1931cf04d95dcd142208362d88699a39a7a90bb
Dan Burton <danburton.email@gmail.com>
2016-08-31 01:00:08 -0700

    Added params, names instead of name

--- src/example/client.cljs
+++ src/example/client.cljs
@@ -7,16 +7,24 @@
 
 ;; https://github.com/omcljs/om/wiki/Quick-Start-(om.next)
 (om/defui HelloWorld
+  static om/IQueryParams
+  (params [_]
+    {:limit 2})
+
   static om/IQuery
   (query [_]
-    [:name])
+    '[(:names {:limit ?limit})])
 
   Object
   (render [this]
+    (let [names (:names (om/props this))]
     (sablono/html
-      [:div (str "Hello, " (:name (om/props this)))])))
+      [:div (str "Hello, "
+                (first names)
+                (when (second names)
+                  (str " and " (second names))))]))))
 
 (def hello (om/factory HelloWorld))
 
 (defn init []
-  (js/ReactDOM.render (hello {:name "Dan"}) (gdom/getElement "app")))
+  (js/ReactDOM.render (hello {:names ["Ag" "Dan"]}) (gdom/getElement "app")))
